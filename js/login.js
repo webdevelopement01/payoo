a047edec9eeb38f5b6bdaae2dad49339e0a69b6b
webdevelopement01 <mdabusayam41@gmail.com>
2024-09-22 19:01:40 +0600

    home screen

--- js/login.js
+++ js/login.js
@@ -4,15 +4,45 @@
 //search: form submit reloading the page
 
 //step 2: set event handler
-document.getElementById('button-login')
-         // step -2: Prevent default behavior (prevent reloading browser)   
-        .addEventListener('click', function(event){
-            event.preventDefault(); //
-            console.log('login button click');
-
-            // step -3: get hte phone number
-            const phoneNumber = document.getElementById('phone-number').value;
-            console.log(phoneNumber);
+// document.getElementById('button-login')
+//          // step -2: Prevent default behavior (prevent reloading browser)   
+//         .addEventListener('click', function(event){
+//             event.preventDefault(); //
+             
+//             // step -3: get hte phone number and pin number
+//             const phoneNumber = document.getElementById('phone-number').value;
+//             const pinNumber = document.getElementById('pin-number').value;
             
+//             console.log(phoneNumber, pinNumber);
 
+//             //step -4: validate phone number
+//             // this is temporary . you should do like this
+//             if(phoneNumber === '5' && pinNumber === '1234'){
+//                 console.log('you are logged in');
+//                 // step -5: allow user to use the website 
+
+//             }
+//             else{
+//                 console.log('wrong phone number');
+//             }
+// })
+
+document.getElementById('button-login').addEventListener('click', function(event){
+    event.preventDefault();
+
+    //get phone number and pin
+    const phoneNumber = document.getElementById('phone-number').value;
+    const pinNumber = document.getElementById('pin-number').value;
+    // console.log(phoneNumber, pinNumber);
+    if(phoneNumber === '5' && pinNumber === '1234'){
+        console.log('your are login')
+        window.location.href = '/home.html'
+    }
+    else{
+        console.log('wrong pin or number')
+    }
+
+
+
+    
 })
